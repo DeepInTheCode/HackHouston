28d376dc66a7a2a45885c5e7f04d227c092d7d81
David Young <davidyoung98@me.com>
2013-09-05 17:38:32 -0500

    Reorganizing into .NET project

---
+++ HackHouston/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace HackHouston.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var apiExplorer = GlobalConfiguration.Configuration.Services.GetApiExplorer();
+            return View(apiExplorer);
+            //return View();
+        }
+    }
+}
